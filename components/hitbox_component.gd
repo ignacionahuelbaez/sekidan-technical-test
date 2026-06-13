5f7ebeff8f7ff6d3359030034e70acc6ea1e157c
ignacionahuelbaez <ignacionahuelbaez45@gmail.com>
2026-06-12 23:20:19 -0300

    Update player

--- components/hitbox_component.gd
+++
@@ -1,9 +0,0 @@
-# Área que inflige daño al entrar en contacto con un HurtboxComponent.
-class_name HitboxComponent
-extends Area2D
-
-@export var damage: float = 10.0
-
-func _ready() -> void:
-	monitoring = true
-	monitorable = true
